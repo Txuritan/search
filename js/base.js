3a0a9d0073a8d216a2044010340e57c6adf0d2e7
Ian Cronkright <txurtian@yahoo.com>
2016-02-05 10:06:40 -0500

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,4 +1,4 @@
-var apiUrl = "http://go.infinise.com/api/2.5/";
+var apiUrl = "https://go.infinise.com/api/2.5/";
 
 
 /*	GOOGLE  ------------------------------------------------------ */
@@ -7,9 +7,9 @@ eng.google = {
 	pageTitle: "Google",
 	logo: "google.png",
 	places: {
-		'Web'    : ["http://www.google.com/search?q=%query%&hl=en",		apiUrl+"?eng=google&timestamp=%time%&q=%query%"],
-		'Images' : ["http://images.google.com/images?q=%query%&hl=en",	apiUrl+"?eng=google&timestamp=%time%&q=%query%"],
-		'Maps'   : ["http://maps.google.com/maps?q=%query%",			false]
+		'Web'    : ["https://www.google.com/search?q=%query%&hl=en",		apiUrl+"?eng=google&timestamp=%time%&q=%query%"],
+		'Images' : ["https://images.google.com/images?q=%query%&hl=en",	apiUrl+"?eng=google&timestamp=%time%&q=%query%"],
+		'Maps'   : ["https://maps.google.com/maps?q=%query%",			false]
 	}
 };
 
@@ -20,8 +20,8 @@ eng.wikipedia = {
 	pageTitle: "Wikipedia",
 	logo: "wikipedia.png",
 	places: {
-		'Search'        : ["http://%lang%.wikipedia.org/wiki/Special:Search?search=%query%&fulltext=Search",	apiUrl+"?eng=wikipedia&timestamp=%time%&q=%query%&hl=%lang%"],
-		'Go to Article' : ["http://%lang%.wikipedia.org/wiki/Special:Search?search=%query%&go=Go",				apiUrl+"?eng=wikipedia&timestamp=%time%&q=%query%&hl=%lang%"],
+		'Search'        : ["https://%lang%.wikipedia.org/wiki/Special:Search?search=%query%&fulltext=Search",	apiUrl+"?eng=wikipedia&timestamp=%time%&q=%query%&hl=%lang%"],
+		'Go to Article' : ["https://%lang%.wikipedia.org/wiki/Special:Search?search=%query%&go=Go",				apiUrl+"?eng=wikipedia&timestamp=%time%&q=%query%&hl=%lang%"],
 	},
 	languages: {
 		'': 'en'
@@ -36,7 +36,7 @@ eng.youtube = {
 	pageTitle: "YouTube",
 	logo: "youtube.png",
 	places: {
-		'Videos' : ["http://www.youtube.com/results?search_query=%query%", apiUrl+"?eng=youtube&timestamp=%time%&q=%query%"]
+		'Videos' : ["https://www.youtube.com/results?search_query=%query%", apiUrl+"?eng=youtube&timestamp=%time%&q=%query%"]
 	},
 };
 
